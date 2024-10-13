335baf8ecae516be6db7b5d6fcefe1b7d6ae4939
Hugo GALAN <hugo.galan8@gmail.com>
2024-10-13 01:46:15 +0100

    adding better parser of options (options passthrough compiler, compiler finder)

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include "src/Lexer.hpp"
 #include "src/Program.hpp"
+#include "src/Options.hpp"
 
 std::string read_all(const std::string &filename)
 {
@@ -22,64 +23,39 @@ std::string read_all(const std::string &filename)
     return content;
 }
 
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        std::cerr << "Not enough arguments provided." << std::endl;
-        return 1;
-    }
-
-    std::string filename = argv[1];
-
-    bool isPreprocessor = false;
-    std::string outputFilename;
-
-    for (int i = 1; i < argc; ++i) {
-        std::string arg = argv[i];
-        if (arg == "-h") {
-            std::cout << "Usage: " << argv[0] << "filename [options]" << std::endl;
-            std::cout << "Options:" << std::endl;
-            std::cout << "  -E          Preprocess only; do not compile" << std::endl;
-            std::cout << "  -o <file>   Place the output into <file>" << std::endl;
-            return 0;
-        }
-        if (arg == "-E") {
-            isPreprocessor = true;
-        } else if (arg == "-o") {
-            if (i + 1 < argc) {
-                outputFilename = argv[++i];
-            } else {
-                    std::cerr << "-o option requires an argument." << std::endl;
-                return 1;
-            }
-        }
-    }
+int main(int argc, char **argv, char **env) {
+    Options options(argc, argv, env);
     try {
-        std::string content = read_all(filename);
+        options.parse();
+    } catch (const std::runtime_error& e) {
+        std::cerr << "JS_CMP: " << e.what() << std::endl;
+        options.printUsage();
+        exit(1);
+    }
+
+    try {
+        std::string content = read_all(options.getFilename());
         Lexer::Lexer lexer(content);
         std::vector<Lexer::Token> tokens = lexer.tokenize();
 
         Lexer::Program program(tokens);
         program.parse();
-        if (isPreprocessor) {
+        if (options.getFlags() & OPTION_FLAG_PREPROCESS) {
             std::cout << program << std::endl;
         } else {
-            if (outputFilename.empty()) {
-                outputFilename = "a.out";
-            }
-            std::string inputFilename = filename + ".cpp";
+            std::string inputFilename = options.getFilename() + std::string(".cpp");
             std::ofstream outputFile(inputFilename);
             outputFile << program;
             outputFile.close();
-            #ifdef _WIN64
-            throw std::runtime_error("Windows 64-bit is not supported.");
-            #elif _WIN32
-            throw std::runtime_error("Windows 32-bit is not supported.");
-            #elif __APPLE__
-            system(("clang++ -g -Ofast -std=c++17 -o " + outputFilename + " " + inputFilename + " -Iincludes -L./ -lJS_CMP 2>/dev/null; if [ $? -ne 0 ]; then echo \"the JavaScript code contain a feature that is not yet implemented.\"; fi").c_str());
-            #elif __linux
-            system(("g++ -g -Ofast -o " + outputFilename + " " + inputFilename +
-                " -Iincludes -L./ -lJS_CMP").c_str());
-            #endif
+            std::cout << "Compiler Found: " << options.getCompiler() << std::endl;
+            std::cout << "Compiling " << options.getFilename() << " to " << options.getOutputFilename() << std::endl;
+            std::string command = options.getCompiler() + (
+                !options.getCompilerArgs().empty() ?
+                    std::string(" ") + options.getCompilerArgs() :
+                    std::string(" -Ofast ")
+            ) + inputFilename + std::string(" -o ") + options.getOutputFilename() + std::string(" -Iincludes -L./ -lJS_CMP");
+            std::cout << "Command: " << command << std::endl;
+            std::system(command.c_str());
         }
     } catch (const std::runtime_error& e) {
         std::cerr << e.what() << std::endl;
---
+++ src/Options.cpp
@@ -0,0 +1,119 @@
+#include "Options.hpp"
+
+Options::Options(int argc, char **argv, char **env) : desc_argv("Allowed options"), compilerArgs()
+{
+    this->argc = argc;
+    this->argv = argv;
+    this->desc_argv.add_options()
+        ("help,h", "produce help message")
+        ("version,v", "print version information")
+        ("preprocess,E", "preprocess only; do not compile")
+        ("CXX,c", po::value<std::string>(), "specify the compiler to use")
+        ("output,o", po::value<std::string>(), "place the output into <file>");
+
+    this->argc = extractCompilerOptions();
+    po::store(po::parse_command_line(argc, argv, this->desc_argv), this->vm_argv);
+    po::notify(this->vm_argv);
+}
+
+void Options::parse()
+{
+    // Parse the command line options
+    if (this->vm_argv.count("help")) {
+        this->printUsage();
+        std::exit(0);
+    }
+    if (this->vm_argv.count("version")) {
+        Options::printVersion();
+        std::exit(0);
+    }
+    if (this->argc < 2) {
+        throw std::runtime_error("No input file.");
+    } else if (!std::filesystem::exists(this->argv[1])) {
+        throw std::runtime_error("File does not exist.");
+    }
+    this->filename = this->argv[1];
+    if (this->vm_argv.count("preprocess")) {
+        this->flags |= OPTION_FLAG_PREPROCESS;
+    }
+    if (this->vm_argv.count("output")) {
+        this->outputFilename = this->vm_argv["output"].as<std::string>();
+    } else {
+        this->outputFilename = this->filename.substr(0, this->filename.find_last_of('.'));
+    }
+
+    // Parse the environment options
+    // TODO: can be cached
+    std::cout << "hello" << std::endl;
+    std::cout << this->vm_argv.count("CXX") << std::endl;
+    std::cout << (std::getenv("CXX") != nullptr) << std::endl;
+
+    if (this->vm_argv.count("CXX")) {
+        this->compiler = this->vm_argv["CXX"].as<std::string>();
+    } else if (std::getenv("CXX") != nullptr) {
+        this->compiler = std::getenv("CXX");
+    } else {
+        auto compiler = findCompiler();
+        std::cout << compiler.has_value() << std::endl;
+        std::cout << compiler.value() << std::endl;
+        if (compiler.has_value()) {
+            this->compiler = compiler.value();
+        } else {
+            throw std::runtime_error("No compiler found. Compiler tested: " + std::accumulate(commonCompilers.begin(), commonCompilers.end(), std::string("")));
+        }
+    }
+    std::cout << this->compiler << std::endl;
+
+}
+
+int Options::extractCompilerOptions()
+{
+    bool hasDoubleDash = false;
+    int nextArgc = argc;
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (hasDoubleDash) {
+            compilerArgs += arg + " ";
+        }
+        if (arg == "--") {
+            hasDoubleDash = true;
+            nextArgc = i;
+        }
+    }
+    return nextArgc;
+}
+
+void Options::printUsage()
+{
+    std::cout << "Usage: js_cmp <file> [options]" << std::endl;
+    this->desc_argv.print(std::cout);
+    std::cout << "  -- <compiler options> Pass the remaining arguments to the compiler" << std::endl;
+}
+
+void Options::printVersion()
+{
+    std::cout << "JS_CMP version " << JS_CMP_VERSION << std::endl;
+}
+
+std::optional<std::string> Options::findCompiler()
+{
+    for (const auto& compiler : commonCompilers) {
+        std::cout << std::string(compiler +
+                #ifdef _WIN32
+                    " --version >nul 2>&1"
+                    #else
+                    " --version > /dev/null 2>&1"
+            #endif
+        ).c_str() << std::endl;
+        if (std::system(std::string(compiler +
+            #ifdef _WIN32
+            " --version >nul 2>&1"
+            #else
+            " --version > /dev/null 2>&1"
+            #endif
+        ).c_str()) == 0) {
+            return std::string(compiler);
+        }
+    }
+    return std::nullopt;
+}

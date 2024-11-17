14c33a2faad1cc613ce4ba0a8bae677e3974b9e0
SimonBandiera <simon.bandiera@epitech.eu>
2024-11-17 15:13:30 +0000

    [+] console to an object

--- includes/class/Console.hpp
+++ includes/class/Console.hpp
@@ -2,30 +2,36 @@
 #define JS_CMP_ALPHA_CONSOLE_HPP
 
 #include <iostream>
+#include <types/objects/JsObject.hpp>
+#include <types/objects/JsFunction.hpp>
 
-class console { // TODO: change initial to capital letter
+class Console : public JS::Object {
 public:
-    template <class... Args>
-    static void log(Args... args) {
-        auto print_with_space = [](const auto& arg, bool is_last) {
-            std::cout << arg;
+    Console() {
+        this->operator[]("log") = JS::Any(std::make_shared<JS::Function>(
+            JS::Function([](const std::vector<JS::Any>& args) -> JS::Any {
+                bool is_last = false;
+                size_t num_args = args.size();
+                size_t counter = 0;
 
-            if (!is_last) {
-                std::cout << " ";
-            }
-        };
+                for (const auto& arg : args) {
+                    print_with_space(arg, ++counter == num_args);
+                }
 
-        bool is_last = false;
-        size_t num_args = sizeof...(args);
-        size_t counter = 0;
-
-        ((print_with_space(args, ++counter == num_args)), ...);
-        std::cout << "\n";
-    }
-
+                std::cout << "\n";
+                return {};
+            })));
+    };
+    ~Console() override = default;
 private:
-    console() = default;
-    ~console() = default;
+    static const std::function<void(const JS::Any&, bool)> print_with_space;
+};
+
+const std::function<void(const JS::Any&, bool)> Console::print_with_space = [](const JS::Any& arg, bool is_last) {
+    std::cout << arg.toString();
+    if (!is_last) {
+        std::cout << " ";
+    }
 };
 
 #endif // JS_CMP_ALPHA_CONSOLE_HPP

fdf36ea9cf301a80b5a21f662fb88bc5cdbc8460
SimonBandiera <simon.bandiera@epitech.eu>
2024-12-28 15:49:30 +0100

    [+] doc

--- includes/class/URI/URIExeption.hpp
+++ includes/class/URI/URIExeption.hpp
@@ -5,10 +5,23 @@
 #include <string>
 #include <utility>
 
+/**
+ * @class URIException
+ * @brief Represents an exception thrown by the URI class.
+ *
+ * The `URIException` class represents an exception thrown by the URI class.
+ */
 class URIException : public std::exception {
 public:
+    /**
+     * @brief Constructs a new URIException with the specified message.
+     * @param message The exception message.
+     */
     explicit URIException(std::string  message) : message(std::move(message)) {}
-
+    /**
+     * @brief Returns the exception message.
+     * @return The exception message.
+     */
     [[nodiscard]] const char* what() const noexcept override {
         return message.c_str();
     }

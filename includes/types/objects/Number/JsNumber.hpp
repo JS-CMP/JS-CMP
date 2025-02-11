a1293b6df8a3339fdc1b31948204eb990fdf726b
SimonBandiera <simon.bandiera@epitech.eu>
2025-02-11 14:13:25 +0000

    [+] add forgotten file

---
+++ includes/types/objects/Number/JsNumber.hpp
@@ -0,0 +1,21 @@
+//
+// Created by Simon BANDIERA on 11/02/2025.
+//
+
+#ifndef JS_CMP_JSNUMBER_HPP
+#define JS_CMP_JSNUMBER_HPP
+
+#include "internals/Object.hpp"
+
+namespace JS {
+class Number : public JS::InternalObject {
+    public:
+        Number();
+        explicit Number(const JS::Any& value);
+
+        ~Number() override = default;
+    };
+}
+
+
+#endif //JS_CMP_JSNUMBER_HPP

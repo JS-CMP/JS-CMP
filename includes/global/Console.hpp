7f7ab50fcbd085bc89061d988296efd8b4f185c2
HForGames <hugo.galan8@gmail.com>
2025-02-15 10:26:17 +0000

    Wip

---
+++ includes/global/Console.hpp
@@ -0,0 +1,39 @@
+#ifndef JS_CMP_ALPHA_CONSOLE_HPP
+#define JS_CMP_ALPHA_CONSOLE_HPP
+
+#include <iostream>
+#include "types/JsAny.hpp"
+
+namespace JS {
+/**
+ * @class Console
+ * @brief A simple logging class that provides static methods to print messages to the console.
+ *
+ * This class encapsulates the logging functionality and provides a single method to log messages
+ * of various types. It ensures that the output is formatted correctly with spaces between arguments.
+ */
+class Console { // TODO: change initial to capital letter
+public:
+    /**
+     * @brief Logs messages to the console.
+     *
+     * This method accepts a variable number of arguments of any type and prints them to the console
+     * with a space separating each argument. A newline is printed at the end.
+     *
+     * @param this The `this` object.
+     * @param args The arguments to log.
+     *
+     * Example usage:
+     * @code
+     * console::log("Hello,", "world!", 42);
+     * @endcode
+     */
+    static void log(const JS::Any& thisArg, const JS::Any& args);
+
+private:
+    Console() = default;  ///< Private constructor to prevent instantiation.
+    ~Console() = default; ///< Private destructor to prevent deletion.
+};
+
+#endif // JS_CMP_ALPHA_CONSOLE_HPP
+} // namespace JS
---
+++ lib/global/console.cpp
@@ -0,0 +1,8 @@
+#include "global/Console.hpp"
+
+namespace JS {
+void Console::log(const JS::Any &thisArg, const JS::Any& args) {
+    int i =
+    std::cout << std::endl;
+}
+} // namespace JS

c0ca7f5f9243ba3d8bd2aa683bfdd28d43fd5a6c
HForGames <hugo.galan8@gmail.com>
2025-06-01 10:36:07 +0100

    fixing boost regex compilation

--- src/Builder.cpp
+++ src/Builder.cpp
@@ -31,7 +31,7 @@ void Builder::compiling(const std::string& inputFilename) const {
     std::string compiler = this->options.getCompiler();
     std::string outputFilename = options.getOutputFilename();
     std::string command = compiler + customArgs + inputFilename + std::string(" -o ") + outputFilename +
-                          std::string(" -Iincludes -L./ -ljscmp $(pkg-config --libs icu-uc icu-i18n) -lboost_regex");
+                          std::string(" -Iincludes -L./ -ljscmp $(pkg-config --libs icu-uc icu-i18n) -DBOOST_REGEX_NO_LIB");
 
     system(command.c_str());
 }

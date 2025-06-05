ce11193c2894ec4f3995d44681a52875f209d5c7
EhGALAN <91669525+EhGALAN@users.noreply.github.com>
2025-06-05 05:20:46 +0000

    Formating Clang-Format and Clang-Tidy

--- includes/utils/Declaration.hpp
+++ includes/utils/Declaration.hpp
@@ -19,15 +19,15 @@
 #define R_PAREN() )
 #define COMMA ,
 
-#define DECLARE_1FUNC(FUNC_HEAD, FUNC_TAIL)                                                                                   \
-    FOR_EACH_CONVERSION_TYPE(FUNC_HEAD L_PAREN(), R_PAREN() FUNC_TAIL)                                                    \
+#define DECLARE_1FUNC(FUNC_HEAD, FUNC_TAIL)                                                                            \
+    FOR_EACH_CONVERSION_TYPE(FUNC_HEAD L_PAREN(), R_PAREN() FUNC_TAIL)                                                 \
     FUNC_HEAD(const JS::Any& value) FUNC_TAIL;
 
-#define DECLARE_2FUNC(FUNC_HEAD, FUNC_TAIL)                                                                                   \
+#define DECLARE_2FUNC(FUNC_HEAD, FUNC_TAIL)                                                                            \
     FOR_EACH_CONVERSION_TYPE(FUNC_HEAD L_PAREN(), COMMA const JS::Any& any R_PAREN() FUNC_TAIL)
 
-#define DECLARE_2FUNC_TWO_WAYS(FUNC_HEAD, FUNC_TAIL)                                                                          \
-    FOR_EACH_CONVERSION_TYPE(FUNC_HEAD L_PAREN(), COMMA const JS::Any& any R_PAREN() FUNC_TAIL)                           \
+#define DECLARE_2FUNC_TWO_WAYS(FUNC_HEAD, FUNC_TAIL)                                                                   \
+    FOR_EACH_CONVERSION_TYPE(FUNC_HEAD L_PAREN(), COMMA const JS::Any& any R_PAREN() FUNC_TAIL)                        \
     FOR_EACH_CONVERSION_TYPE(FUNC_HEAD L_PAREN() const JS::Any& any COMMA, R_PAREN() FUNC_TAIL)
 
 #endif // DECLARATION_HPP
